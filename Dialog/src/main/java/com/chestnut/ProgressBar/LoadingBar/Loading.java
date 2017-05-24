a2c168881b7678cc763a1ae1e27bbcb28858b3c6
Chestnut <974920378@qq.com>
2017-05-24 23:03:21 +0800

    2017年5月24日23:02:40：宿舍： 1. Loading 2. ProLoadingDialog

---
+++ Dialog/src/main/java/com/chestnut/ProgressBar/LoadingBar/Loading.java
@@ -0,0 +1,19 @@
+package com.chestnut.ProgressBar.LoadingBar;
+
+/**
+ * <pre>
+ *     author: Chestnut
+ *     blog  : http://www.jianshu.com/u/a0206b5f4526
+ *     time  : 2017/5/24 22:54
+ *     desc  :
+ *     thanks To:
+ *     dependent on:
+ *     update log:
+ * </pre>
+ */
+
+public class Loading {
+
+    
+
+}
